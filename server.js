33f26af3bc9051729a186ccb98445beaed5b70f8
Mudassir Malik <Malikjrw147@gmail.com>
2022-10-05 17:26:20 +0500

    ngrok setup completed

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ var bodyParser = require('body-parser');
 const app = express();
 const http = require('http');
 const cors = require('cors');
+const ngrok = require('ngrok');
 const initPeer = require('./src/peer');
 const initSocketIO = require('./src/socket');
 const user_routes = require('./src/user/users.routes');
 const { get_network_ip } = require('./src/utils');
 const localNetworkIP = get_network_ip();
-const PORT = process.env.SAME_ORIGIN_STABLE_LOCAL_BRANCH_SERVER_AND_SOCKET_PORT || 8191;
+const PORT = process.env.NGROK_BRANCH_SERVER_AND_SOCKET_PORT || 8191;
 require('./src/config/database');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
@@ -24,6 +25,23 @@ const server = http.createServer(app);
 initSocketIO(server);
 // INIT PEER SERVER CONNECTIONS
 initPeer(localNetworkIP);
-server.listen(PORT, `${localNetworkIP}`, () => {
+
+const ngrok_options = {
+  proto: 'http', // http|tcp|tls, defaults to http
+  addr: PORT, // port or network address, defaults to 80
+  authtoken: `${process.env.NGROK_AUTH_TOKEN}`, // your authtoken from ngrok.com
+  // subdomain: 'alex', // reserved tunnel name https://alex.ngrok.io
+  // auth: 'user:pwd', // http basic authentication for tunnel
+  // region: 'us', // one of ngrok regions (us, eu, au, ap, sa, jp, in), defaults to us
+  // configPath: '~/git/project/ngrok.yml', // custom path for ngrok config file
+  // binPath: path => path.replace('app.asar', 'app.asar.unpacked'), // custom binary path, eg for prod in electron
+  // onStatusChange: status => { }, // 'closed' - connection is lost, 'connected' - reconnected
+  // onLogEvent: data => { }, // returns stdout messages from ngrok process
+};
+(async function () {
+  const url = await ngrok.connect(ngrok_options);
+  console.log('__ngrok_public_url__', url);
+})();
+server.listen(PORT, () => {
   console.log(`Server is listening at ipv4 http://${localNetworkIP}:${PORT}/`);
 });
